d1d9bf2d68d07b99aa92cb9efa5a0cc170fc3b4d
Ben Rowe <ben.rowe.83@gmail.com>
2016-06-30 08:00:51 +1000

    Add base Exception class for Widget Exceptions

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Arrilot\Widgets\Exceptions;
+
+use Exception as BaseException;
+
+/**
+ * Represents an exception in the widget system
+ *
+ * @package Arrilot\Widgets
+ */
+class Exception extends BaseException
+{
+}
